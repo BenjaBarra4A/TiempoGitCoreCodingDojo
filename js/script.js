837b09b3dca1309f8c42404ca0570c5f81b34c9e
Benjamin Barra <benjamin.barra.diaz@institutocomercialosorno.cl>
2024-04-16 08:11:54 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,106 @@
+alert("Cargando Reporte Del Clima");
+var botoneliminar = document.getElementById("botonA");
+
+    botoneliminar.addEventListener("click", function() {
+        galletas.remove();
+});
+var cambiarRegion = document.getElementById("Arg");
+    
+    cambiarRegion.addEventListener("click", function() {
+        localidad.innerText = "Buenos Aires";
+        hoy.innerText = "Hoy";
+        tipo1.innerText = "Nublado";
+        grados1.innerText = "22°C° 15°C°";
+        Mañana.innerText = "Mañana";
+        tipo2.innerText = "Lluvioso";
+        grados2.innerText = "20°C° 13°C°";
+        Miercoles.innerText = "Miercoles";
+        tipo3.innerText = "Tormentas";
+        grados3.innerText = "18°C° 11°C°";
+        Jueves.innerText = "Jueves";
+        tipo4.innerText = "Parcialmente Nublado";
+        grados4.innerText = "21°C° 14°C°";
+        Viernes.innerText = "Viernes";
+        tipo5.innerText = "Soleado";
+        grados5.innerText = "24°C° 17°C°";
+});
+var cambiarRegion = document.getElementById("Mx");
+    
+    cambiarRegion.addEventListener("click", function() {
+        localidad.innerText = "Ciudad de Mexico";
+        hoy.innerText = "Hoy";
+        tipo1.innerText = "Nublado";
+        grados1.innerText = "18°C° 6°C°";
+        Mañana.innerText = "Mañana";
+        tipo2.innerText = "Lluvioso";
+        grados2.innerText = "15°C° 3°C°";
+        Miercoles.innerText = "Miercoles";
+        tipo3.innerText = "Tormentas";
+        grados3.innerText = "18°C° 5°C°";
+        Jueves.innerText = "Jueves";
+        tipo4.innerText = "Parcialmente Nublado";
+        grados4.innerText = "20°C° 12°C°";
+        Viernes.innerText = "Viernes";
+        tipo5.innerText = "Soleado";
+        grados5.innerText = "30°C° 18°C°";
+});
+var cambiarRegion = document.getElementById("Cl");
+    
+    cambiarRegion.addEventListener("click", function() {
+        localidad.innerText = "Santiago";
+        hoy.innerText = "Hoy";
+        tipo1.innerText = "Nublado";
+        grados1.innerText = "20°C° 4°C°";
+        Mañana.innerText = "Mañana";
+        tipo2.innerText = "Lluvioso";
+        grados2.innerText = "19°C° 7°C°";
+        Miercoles.innerText = "Miercoles";
+        tipo3.innerText = "Tormentas";
+        grados3.innerText = "16°C° 2°C°";
+        Jueves.innerText = "Jueves";
+        tipo4.innerText = "Parcialmente Nublado";
+        grados4.innerText = "19°C° 6°C°";
+        Viernes.innerText = "Viernes";
+        tipo5.innerText = "Soleado";
+        grados5.innerText = "32°C° 15°C°";
+});
+var cambiarRegion = document.getElementById("Br");
+    
+    cambiarRegion.addEventListener("click", function() {
+        localidad.innerText = "Sao Paulo";
+        hoy.innerText = "Hoy";
+        tipo1.innerText = "Nublado";
+        grados1.innerText = "15°C° 5°C°";
+        Mañana.innerText = "Mañana";
+        tipo2.innerText = "Lluvioso";
+        grados2.innerText = "18°C° 5°C°";
+        Miercoles.innerText = "Miercoles";
+        tipo3.innerText = "Tormentas";
+        grados3.innerText = "19°C° 7°C°";
+        Jueves.innerText = "Jueves";
+        tipo4.innerText = "Parcialmente Nublado";
+        grados4.innerText = "20°C° 8°C°";
+        Viernes.innerText = "Viernes";
+        tipo5.innerText = "Soleado";
+        grados5.innerText = "36°C° 23°C°";
+});
+var cambiarRegion = document.getElementById("Ec");
+    
+    cambiarRegion.addEventListener("click", function() {
+        localidad.innerText = "Quito";
+        hoy.innerText = "Hoy";
+        tipo1.innerText = "Nublado";
+        grados1.innerText = "19°C° 8°C°";
+        Mañana.innerText = "Mañana";
+        tipo2.innerText = "Lluvioso";
+        grados2.innerText = "15°C° 5°C°";
+        Miercoles.innerText = "Miercoles";
+        tipo3.innerText = "Tormentas";
+        grados3.innerText = "19°C° 10°C°";
+        Jueves.innerText = "Jueves";
+        tipo4.innerText = "Parcialmente Nublado";
+        grados4.innerText = "23°C° 13°C°";
+        Viernes.innerText = "Viernes";
+        tipo5.innerText = "Soleado";
+        grados5.innerText = "29°C° 15°C°";
+});
